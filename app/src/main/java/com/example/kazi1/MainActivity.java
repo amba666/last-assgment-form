c4f922fa5f65785fcfffd7cea1831e6352a6fe67
Godeye <67413568+amba666@users.noreply.github.com>
2023-05-29 16:18:10 +0300

    form with spinner

---
+++ app/src/main/java/com/example/kazi1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.kazi1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Spinner spinner =findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.gander, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+}
